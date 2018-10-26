064526a523c4906f0fd9852aed94521128e13a4c
Sebastian Turen <seba@turensoft.com>
2018-10-27 01:01:26 +0900

    test

--- WEB-INF/src/dbConnect/DBConfig.java
+++ WEB-INF/src/dbConnect/DBConfig.java
@@ -8,12 +8,12 @@ package com.artOfWar.dbConnect;
 public interface DBConfig {
 	
 	//jdbc information
-	public static final String JDBC_DRIVER 	= ""; //com.mysql.jdbc.Driver
-	public static final String JDBC_URL 	= ""; //jdbc:mysql://127.0.0.1/
+	public static final String JDBC_DRIVER 	= "test"; //com.mysql.jdbc.Driver
+	public static final String JDBC_URL 	= "test"; //jdbc:mysql://127.0.0.1/
 	
 	//DB information
-	public static final String DB_NAME		= "";
-	public static final String DB_USER		= "";
-	public static final String DB_PASSWORD	= "";
+	public static final String DB_NAME		= "test";
+	public static final String DB_USER		= "test";
+	public static final String DB_PASSWORD	= "test";
 	
 }
