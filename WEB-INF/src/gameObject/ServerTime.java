b7476bb6311604d3581caab2f26d57b69cb37209
Sebastián Turén <sebaturen@gmail.com>
2020-01-25 23:02:22 -0300

    add auto check server maintenance time
    Fix #3

--- WEB-INF/src/gameObject/ServerTime.java
+++ WEB-INF/src/gameObject/ServerTime.java
@@ -22,10 +22,12 @@ public class ServerTime
     public static final String EST_TIME_ZONE = "";
     public static final String TIME_ZONE = "PST";
     private static ServerTime svObject;
-    
+    public static final int MAINTENANCE_DAY = Calendar.TUESDAY;
+    public static final int MAINTENANCE_HOUR = 7;
+
     //Atribute
     private final String timeZone;
-    
+
     public ServerTime()
     {
         switch(ServerTime.TIME_ZONE)
@@ -33,10 +35,10 @@ public class ServerTime
             case "MST": this.timeZone = MST_TIME_ZONE; break;
             case "CST": this.timeZone = CST_TIME_ZONE; break;
             case "EST": this.timeZone = EST_TIME_ZONE; break;
-            default: this.timeZone = PST_TIME_ZONE; break;                
+            default: this.timeZone = PST_TIME_ZONE; break;
         }
     }
-        
+
     /**
      *  Get current server time.
      * @return Date time
@@ -59,16 +61,46 @@ public class ServerTime
             Logs.errorLog(FactionAssaultControl.class, "Fail to get current time");
         }
         return currentTime;
-    }    
-    
+    }
+
     /**
      * Get last maintenance time in long
-     * 
-     * @return 
+     *
+     * @return
      */
     public static long getLastResetTime()
     {
-        return 1579586219000L;
+        Date sTime = getServerTime();
+        Calendar sCalendar = Calendar.getInstance();
+        sCalendar.setTimeZone(TimeZone.getTimeZone(svObject.getTimeZone()));
+        sCalendar.setTime(sTime);
+
+        if (sCalendar.get(Calendar.DAY_OF_WEEK) > MAINTENANCE_DAY) {
+            return getCurrentMaintenance(sCalendar);
+        } else if (sCalendar.get(Calendar.DAY_OF_WEEK) == MAINTENANCE_DAY) {
+            if (sCalendar.get(Calendar.HOUR_OF_DAY) >= MAINTENANCE_HOUR) {
+                return getCurrentMaintenance(sCalendar);
+            } else {
+                return getPreviewMaintenance(sCalendar);
+            }
+        } else {
+            return getPreviewMaintenance(sCalendar);
+        }
+    }
+
+    public static long getPreviewMaintenance(Calendar serverCalendar) {
+        while (serverCalendar.get(Calendar.DAY_OF_WEEK) != MAINTENANCE_DAY) {
+            serverCalendar.add(Calendar.DAY_OF_MONTH, -1);
+        }
+        serverCalendar.set(Calendar.HOUR_OF_DAY, MAINTENANCE_HOUR);
+        return serverCalendar.getTimeInMillis();
+
+    }
+
+    public static long getCurrentMaintenance(Calendar serverCalendar) {
+        serverCalendar.set(Calendar.DAY_OF_WEEK, MAINTENANCE_DAY);
+        serverCalendar.set(Calendar.HOUR_OF_DAY, MAINTENANCE_HOUR);
+        return serverCalendar.getTimeInMillis();
     }
     
     /**
